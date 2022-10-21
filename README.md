44635ce018789b4a793526f5669c4dfbf0365ddc
Dominik Lenz <dominik@notabot.cc>
2022-10-21 19:15:46 +0200

    project init

---
+++ README.md
@@ -0,0 +1,34 @@
+## Micronaut 3.7.2 Documentation
+
+- [User Guide](https://docs.micronaut.io/3.7.2/guide/index.html)
+- [API Reference](https://docs.micronaut.io/3.7.2/api/index.html)
+- [Configuration Reference](https://docs.micronaut.io/3.7.2/guide/configurationreference.html)
+- [Micronaut Guides](https://guides.micronaut.io/index.html)
+---
+
+- [Jib Gradle Plugin](https://plugins.gradle.org/plugin/com.google.cloud.tools.jib)
+- [Shadow Gradle Plugin](https://plugins.gradle.org/plugin/com.github.johnrengelman.shadow)
+## Feature management documentation
+
+- [Micronaut Management documentation](https://docs.micronaut.io/latest/guide/index.html#management)
+
+
+## Feature openapi documentation
+
+- [Micronaut OpenAPI Support documentation](https://micronaut-projects.github.io/micronaut-openapi/latest/guide/index.html)
+
+- [https://www.openapis.org](https://www.openapis.org)
+
+
+## Feature http-client documentation
+
+- [Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
+
+
+## Feature kubernetes documentation
+
+- [Micronaut Kubernetes Support documentation](https://micronaut-projects.github.io/micronaut-kubernetes/latest/guide/index.html)
+
+- [https://kubernetes.io/docs/home/](https://kubernetes.io/docs/home/)
+
+
---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("io.micronaut.application") version "3.6.2"
+    id("com.google.cloud.tools.jib") version "2.8.0"
+}
+
+version = "0.1"
+group = "com.aixtra"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    annotationProcessor("io.micronaut:micronaut-http-validation:3.7.1")
+    annotationProcessor("io.micronaut.openapi:micronaut-openapi:4.5.1")
+    implementation("io.micronaut:micronaut-http-client:3.7.1")
+    implementation("io.micronaut:micronaut-jackson-databind:3.7.1")
+    implementation("io.micronaut:micronaut-management:3.7.1")
+    implementation("io.swagger.core.v3:swagger-annotations:2.2.4")
+    implementation("jakarta.annotation:jakarta.annotation-api:2.1.0")
+    runtimeOnly("ch.qos.logback:logback-classic:1.4.4")
+    implementation("io.micronaut:micronaut-validation:3.7.1")
+}
+
+
+application {
+    mainClass.set("com.aixtra.couchcode.Application")
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("17")
+    targetCompatibility = JavaVersion.toVersion("17")
+}
+
+tasks {
+    jib {
+        to {
+            image = "gcr.io/myapp/jib-image"
+        }
+    }
+}
+graalvmNative.toolchainDetection.set(false)
+micronaut {
+    runtime("netty")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("com.aixtra.couchcode.*")
+    }
+}
+
+
+
