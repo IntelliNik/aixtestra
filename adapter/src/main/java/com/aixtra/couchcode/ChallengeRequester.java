12b4a1c5363ae26cad56bc76f9f15980f56de094
Dominik Lenz <dominik@notabot.cc>
2022-10-22 18:13:37 +0200

    adds challenge requester

--- adapter/src/main/java/com/aixtra/couchcode/ChallengeRequester.java
+++ adapter/src/main/java/com/aixtra/couchcode/ChallengeRequester.java
@@ -5,7 +5,9 @@ import io.micronaut.context.annotation.Context;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.util.retry.Retry;
 
+import java.time.Duration;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -24,7 +26,14 @@ public class ChallengeRequester {
         ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
         executor.scheduleAtFixedRate(() -> {
             LOGGER.info("Registering for next rating run");
-            rating.register().subscribe();
+            rating.register()
+                    .retryWhen(Retry.backoff(5, Duration.ofSeconds(2))
+                            .doBeforeRetry(signal -> LOGGER.warn("Error while registering for rating {} times:  {}, retrying...", signal.totalRetries(), signal.failure().getMessage()))
+                    ).doOnError((err) -> LOGGER.error("Could not register for rating", err))
+                    .doOnNext((res) -> LOGGER.info("Registered for next rating run"))
+                    .subscribe();
+
+
         }, 0, 30, TimeUnit.MINUTES);
 
     }
